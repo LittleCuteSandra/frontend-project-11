f78fed51bd919c8532ac7b5b60d3703f83345cee
Королева Александра <sasha.sasha.koroleva@mail.ru>
2024-11-10 23:08:12 +0300

    add src/locale/ src/parser.js

---
+++ src/locale/index.js
@@ -0,0 +1,13 @@
+export default {
+  ru: {
+    translation: {
+      networkErr: 'Ошибка сети',
+      emptyFieldErr: 'Поле не должно быть пустым',
+      notValidLinkErr: 'Ссылка должна быть валидным URL',
+      unknownErr: 'Неизвестная ошибка. Что-то пошло не так.',
+      existRSSErr: 'RSS уже существует',
+      notValidRSSErr: 'Ресурс не содержит валидный RSS',
+      successRSS: 'RSS успешно загружен',
+    },
+  },
+};
---
+++ src/parser.js
@@ -0,0 +1,37 @@
+const parsePosts = (posts) => {
+  const postsArray = [];
+  posts.forEach((post) => {
+    const title = post.querySelector('title').textContent;
+    const description = post.querySelector('description').textContent;
+    const link = post.querySelector('link').textContent;
+    postsArray.push({
+      title,
+      description,
+      link,
+    });
+  });
+
+  return postsArray;
+};
+
+const parse = (data) => {
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(data, 'application/xml');
+
+  const error = xmlDoc.querySelector('parsererror');
+  if (error) {
+    // КАК ТО ПО ДРУГОМУ ОФОРМИТЬ ОТВЕТ
+    return error.textContent;
+  }
+  const title = xmlDoc.querySelector('title').textContent;
+  const description = xmlDoc.querySelector('description').textContent;
+  console.log(typeof xmlDoc.querySelectorAll('item'), xmlDoc.querySelectorAll('item')[0]);
+  const posts = parsePosts(xmlDoc.querySelectorAll('item'));
+  return {
+    title,
+    description,
+    posts,
+  };
+};
+
+export default parse;
