1a143ea6ff71bee53458bf56817f5d224653e33a
Королева Александра <sasha.sasha.koroleva@mail.ru>
2024-11-12 22:39:53 +0300

    change src/locale/index.js

--- src/locale/index.js
+++ src/locale/index.js
@@ -2,12 +2,14 @@ export default {
   ru: {
     translation: {
       networkErr: 'Ошибка сети',
-      emptyFieldErr: 'Поле не должно быть пустым',
       notValidLinkErr: 'Ссылка должна быть валидным URL',
       unknownErr: 'Неизвестная ошибка. Что-то пошло не так.',
       existRSSErr: 'RSS уже существует',
       notValidRSSErr: 'Ресурс не содержит валидный RSS',
       successRSS: 'RSS успешно загружен',
+      titleView: 'Просмотр',
+      titlePosts: 'Посты',
+      titleFeeds: 'Фиды',
     },
   },
 };
